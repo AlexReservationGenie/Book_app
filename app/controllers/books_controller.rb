884168a500c866beea94097eda08cc599f20f45a
alexreservationgenie <alex@reservationgenie.com>
2017-07-19 18:12:58 -0500

    layout start

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,16 @@
 class BooksController < ApplicationController
+  before_action :find_book
 
   def index
-    
+
   end
 
   def new
 
   end
+
+  private
+    def find_book
+      @book = Book.find(params[:id])
+    end
 end
