3466b08583933ec3700bb6ba771e3f602d8b1c2a
Alex Key <alex@reservationgenie.com>
2019-12-22 20:05:45 -0600

    only admins can create books

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authorize_admin
+    if current_user
+      return if current_user.admin?
+    end
+    redirect_to root_path, alert: 'Admins only!'
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  belongs_to :user
+  #belongs_to :user
   has_many :reviews
 
   has_attached_file :image, styles: { medium: "400x600>" }, default_url: "/images/:style/missing.png"
